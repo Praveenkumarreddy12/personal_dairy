e7c8fa13081379ba8ed51f2751c1e59a5180d929
praveen <149228650+Praveenkumarreddy12@users.noreply.github.com>
2025-08-15 10:34:48 +0530

    Update mysql database

--- dream/settings.py
+++ dream/settings.py
@@ -80,13 +80,23 @@ WSGI_APPLICATION = 'dream.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # Use MySQL as the database backend
+        'NAME': 'note_mydairy',         # Replace with your database name
+        'USER': 'root',         # Replace with your MySQL username
+        'PASSWORD': 'root',          # Replace with your MySQL password
+        'HOST': '127.0.0.1',                  # Database host (use '127.0.0.1' or IP address)
+        'PORT': '3306',                       # Default MySQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -146,3 +156,4 @@ EMAIL_PORT = 587  # Common port for TLS
 EMAIL_USE_TLS = True  # Use TLS
 EMAIL_HOST_USER = 'dmydairy@gmail.com'  # Your email address
 EMAIL_HOST_PASSWORD = 'zhag eefe xxjj eyky'  # Your email password
+
